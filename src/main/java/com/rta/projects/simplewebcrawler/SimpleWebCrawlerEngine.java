2c7207e5b87e77e09ff1cd2655122ee63f01b0e0
Rafał Tarwacki <rafal.tarwacki@poczta.fm>
2019-02-03 23:59:10 +0100

    first unit test for simple web crawler

--- src/main/java/com/rta/projects/simplewebcrawler/SimpleWebCrawlerEngine.java
+++ src/main/java/com/rta/projects/simplewebcrawler/SimpleWebCrawlerEngine.java
@@ -1,5 +1,22 @@
 package com.rta.projects.simplewebcrawler;
 
-public class SimpleWebCrawlerEngine {
+import java.io.IOException;
+import java.net.UnknownHostException;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class SimpleWebCrawlerEngine {
+	public static boolean checkUrlConnetion(String url) {
+		try {
+			Document doc = Jsoup.connect(url).get();
+			return true;
+		} catch (UnknownHostException e) {
+			System.out.println(e);
+			return false;
+		} catch (IOException e) {
+			System.out.println(e);
+			return false;
+		}
+	}
 }
--- src/test/java/com/rta/projects/simplewebcrawler/SimpleWebCrawlerEngineTest.java
+++ src/test/java/com/rta/projects/simplewebcrawler/SimpleWebCrawlerEngineTest.java
@@ -7,32 +7,34 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple web crawler.
  */
-public class SimpleWebCrawlerEngineTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public SimpleWebCrawlerEngineTest( String testName )
-    {
-        super( testName );
-    }
+public class SimpleWebCrawlerEngineTest extends TestCase {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName name of the test case
+	 */
+	public SimpleWebCrawlerEngineTest(String testName) {
+		super(testName);
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( SimpleWebCrawlerEngineTest.class );
-    }
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(SimpleWebCrawlerEngineTest.class);
+	}
 
-    /**
-     * Tests
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	/**
+	 * Tests
+	 */
+	public void testCorrectConnetion() {
+		String url = "http://wiprodigital.com";
+		assertTrue(SimpleWebCrawlerEngine.checkUrlConnetion(url));
+	}
+
+	public void testIncorrectConnetion() {
+		String url = "http://wipro12345digital.com";
+		assertFalse(SimpleWebCrawlerEngine.checkUrlConnetion(url));
+	}
+
 }
